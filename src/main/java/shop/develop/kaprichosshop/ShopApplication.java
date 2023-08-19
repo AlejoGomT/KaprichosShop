936509c85fd12d905c02f75dbca28b4019d43fdd
Alejo Gomez T <alejogomezt2000@gmail.com>
2023-08-19 00:38:27 -0500

    Added connection between buttons: Close App, Return/Back, Register and Route Button (Client, Sale, Product)

--- src/main/java/shop/develop/kaprichosshop/ShopApplication.java
+++ src/main/java/shop/develop/kaprichosshop/ShopApplication.java
@@ -1,18 +1,43 @@
 package shop.develop.kaprichosshop;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class ShopApplication extends Application {
+    private double cursorX;
+    private double cursorY;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ShopApplication.class.getResource("kprichos-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent fxmlLoader = FXMLLoader.load(getClass().getResource("kprichos-view.fxml"));
+
+        fxmlLoader.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                cursorX = event.getSceneX();
+                cursorY = event.getSceneY();
+            }
+        });
+        fxmlLoader.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - cursorX);
+                stage.setY(event.getScreenY() - cursorY);
+            }
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        Scene scene = new Scene(fxmlLoader);
+        stage.setTitle("KprichosShop");
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/shop/develop/kaprichosshop/ShopController.java
+++ src/main/java/shop/develop/kaprichosshop/ShopController.java
@@ -1,14 +1,93 @@
 package shop.develop.kaprichosshop;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class ShopController {
+    //Controls elements from fxml
     @FXML
-    private Label welcomeText;
+    public AnchorPane pageHome, salesContainer, salesDataPage, salesRegisterPage,
+            productsContainer, productsDataPage, productsRegisterPage, clientsContainer,
+            clientsDataPage, clientsRegisterPage;
+    public Pane modalPaneOfSale;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    //Function for close the application
+    public void clickCloseButton(ActionEvent event){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    //Button Back
+    public void clickBackButton(ActionEvent event){
+        if(salesContainer.isVisible()){
+            if(salesDataPage.isVisible()){
+                salesContainer.setVisible(false);
+                pageHome.setVisible(true);
+            }
+            if(salesRegisterPage.isVisible()){
+                salesRegisterPage.setVisible(false);
+                salesDataPage.setVisible(true);
+            }
+        }
+        if(productsContainer.isVisible()){
+            if(productsDataPage.isVisible()){
+                productsContainer.setVisible(false);
+                pageHome.setVisible(true);
+            }
+            if(productsRegisterPage.isVisible()){
+                productsRegisterPage.setVisible(false);
+                productsDataPage.setVisible(true);
+            }
+        }
+        if(clientsContainer.isVisible()){
+            if(clientsDataPage.isVisible()){
+                clientsContainer.setVisible(false);
+                pageHome.setVisible(true);
+            }
+            if(clientsRegisterPage.isVisible()){
+                clientsRegisterPage.setVisible(false);
+                clientsDataPage.setVisible(true);
+            }
+        }
+    }
+
+    //Actions buttons from modal pane of sale
+    public void addProductModal(ActionEvent event){
+        modalPaneOfSale.setVisible(true);
+    }
+    public void closeProductModal(ActionEvent event){
+        modalPaneOfSale.setVisible(false);
+    }
+
+
+    //Routes Buttons: Sales, Clients and Products
+    public void clickSalesButton(ActionEvent event){
+        pageHome.setVisible(false);
+        salesContainer.setVisible(true);
+    }
+    public void clickProductsButton(ActionEvent event){
+        pageHome.setVisible(false);
+        productsContainer.setVisible(true);
+    }
+    public void clickClientsButton(ActionEvent event){
+        pageHome.setVisible(false);
+        clientsContainer.setVisible(true);
+    }
+
+    //Register Buttons: Sales, Clients and Products
+    public void clickRegisterSale(ActionEvent event){
+        salesDataPage.setVisible(false);
+        salesRegisterPage.setVisible(true);
+    }
+    public void clickRegisterProduct(ActionEvent event){
+        productsDataPage.setVisible(false);
+        productsRegisterPage.setVisible(true);
+    }
+    public void clickRegisterClient(ActionEvent event){
+        clientsDataPage.setVisible(false);
+        clientsRegisterPage.setVisible(true);
     }
 }
